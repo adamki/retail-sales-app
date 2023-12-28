1390182da485bbf4e55cfa9b14ee5b9212061d3c
adamki <adajensen@gmail.com>
2023-12-28 08:49:25 -0800

    init commit. adds main package suite

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const App = (): JSX.Element =>  {
+
+
+  return (
+    <div className="h-screen flex flex-col">
+    </div>
+  );
+}
+
+export default App;
---
+++ src/colors.ts
@@ -0,0 +1,5 @@
+export const darkBlue = "blue-900"
+export const white = "white"
+export const lightGray = "gray-200"
+export const gray = "gray-400"
+
